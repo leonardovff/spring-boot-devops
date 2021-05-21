d41e602295fe54165203835006e32574f74e5a39
leonardovff <leonardovff@gmail.com>
2021-05-21 01:02:55 -0300

    Add option of use docker secret in the project

---
+++ src/main/java/com/example/springboot/env/DockerSecretsDatabasePasswordProcessor.java
@@ -0,0 +1,39 @@
+package com.example.springboot.env;
+
+// import org.slf4j.Logger;
+// import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.env.EnvironmentPostProcessor;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.PropertiesPropertySource;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
+import org.springframework.util.StreamUtils;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.util.Properties;
+
+public class DockerSecretsDatabasePasswordProcessor implements EnvironmentPostProcessor {
+    // private final Logger logger = LoggerFactory.getLogger(DockerSecretsDatabasePasswordProcessor.class);
+
+    @Override
+    public void postProcessEnvironment(ConfigurableEnvironment environment, SpringApplication application) {
+        Resource resource = new FileSystemResource("/run/secrets/dbpassword");
+
+        if (resource.exists()) {
+            try {
+                // if (logger.isInfoEnabled()) {
+                    //     logger.info("Using database password from injected Docker secret file");
+                    // }
+                    
+                String dbPassword = StreamUtils.copyToString(resource.getInputStream(), Charset.defaultCharset());
+                Properties props = new Properties();
+                props.put("spring.datasource.password", dbPassword.trim());
+                environment.getPropertySources().addFirst(new PropertiesPropertySource("dbProps", props));
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
