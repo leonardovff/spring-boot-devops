544c26ec8fbab576fb7bb86fd0ddb9570dd8244a
wanessaramos <wanessinha_ramos@yahoo.com.br>
2021-06-16 23:34:09 -0300

    environment variable

--- docker-config/app/docker-startup.sh
+++ docker-config/app/docker-startup.sh
@@ -1,6 +1,7 @@
-$ ./wait-for-it.sh -t 0 mysql:3306 -- echo "mysql ok"
+./wait-for-it.sh -t 0 $ MYSQL_HOST: MYSQL_INTERNAL_PORT -- echo "mysql ok"
 wait-for-it.sh: waiting for  mysql:3306 without a timeout
 wait-for-it.sh: mysql:3306 is available after 0 seconds
 mysql ok
 
 java -jar /usr/local/lib/demo.jar
+
