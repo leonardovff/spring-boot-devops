08e49af1d56b43c8f2833e993454a137695d9f90
Leonardo Victor <leonardovff@gmail.com>
2021-06-17 12:43:07 +0000

    Update docker-startup.sh

--- docker-config/app/docker-startup.sh
+++ docker-config/app/docker-startup.sh
@@ -1,7 +1,4 @@
 wait-for-it.sh -t 0 $MYSQL_HOST:$MYSQL_INTERNAL_PORT -- echo "mysql ok"
-wait-for-it.sh: waiting for  mysql:3306 without a timeout
-wait-for-it.sh: mysql:3306 is available after 0 seconds
-mysql ok
 
 java -jar /usr/local/lib/demo.jar
 
